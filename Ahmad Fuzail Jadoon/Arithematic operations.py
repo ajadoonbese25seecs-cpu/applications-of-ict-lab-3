fb5f3e428ce62a166110f80e4edf13d8957a67f9
aka.ahmad <ajadoon.bese25seecs@seecs.edu.pk>
2025-09-22 13:54:53 +0500

    Add files via upload

---
+++ Ahmad Fuzail Jadoon/Arithematic operations.py
@@ -0,0 +1,9 @@
+x = 10
+y = 5
+print("Addition", x + y)
+print("Subtraction", x - y)
+print("Multiplication", x * y)
+print("Floor division(integer result)", x // y)
+print("Division(float)", x / y)
+print("Modulus(remainder)", x % y)
+print("Exponent(power)", x ** y)
